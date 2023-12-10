737309da1a197145c25a92b1a0ff2662d5bd06f5
Manasa <manasagangula2001@gmail.com>
2023-12-10 14:05:28 +0530

    Fix stock price calculation in client3.py

--- client3.py
+++ client3.py
@@ -32,28 +32,51 @@ N = 500
 def getDataPoint(quote):
     """ Produce all the needed values to generate a datapoint """
     """ ------------- Update this function ------------- """
+    """Produce all the needed values to generate a datapoint"""
     stock = quote['stock']
     bid_price = float(quote['top_bid']['price'])
     ask_price = float(quote['top_ask']['price'])
-    price = bid_price
+
+    # Compute the correct stock price using the formula: (bid_price + ask_price) / 2
+    price = (bid_price + ask_price) / 2
+
     return stock, bid_price, ask_price, price
 
 
 def getRatio(price_a, price_b):
     """ Get ratio of price_a and price_b """
     """ ------------- Update this function ------------- """
-    return 1
 
 
+     # Handle the case where price_b could be zero to avoid division by zero
+    if price_b == 0:
+        return float('inf')
+
+    # Calculate the ratio of price_a to price_b
+    return price_a / price_b
+
+
+
+
+# Main
 # Main
 if __name__ == "__main__":
     # Query the price once every N seconds.
     for _ in iter(range(N)):
         quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
 
-        """ ----------- Update to get the ratio --------------- """
+        # Store data points for each stock
+        data_points = []
         for quote in quotes:
             stock, bid_price, ask_price, price = getDataPoint(quote)
             print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
 
-        print("Ratio %s" % getRatio(price, price))
+            # Store the data point in a tuple
+            data_points.append((stock, price))
+
+        # Calculate and print the ratio for the stored data points
+        for stock_a, price_a in data_points:
+            for stock_b, price_b in data_points:
+                if stock_a != stock_b:
+                    ratio = getRatio(price_a, price_b)
+                    print("Ratio %s/%s: %s" % (stock_a, stock_b, ratio))
